2cc49f591d8886fbeb60cb0ff99eb5a2ddd32f28
Korna <artem-koma@yandex.ru>
2016-05-22 00:56:08 +0600

    Rebuilded func sort with new method and added function exchange to improve the code

--- src/functions.c
+++ src/functions.c
@@ -1,22 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <windows.h>
 
-int schr(char str[], char ch) {
+int exchange (char *ptr[], int j){
+	char *chtmp;
+	char *chtmp2;
+	chtmp2 = ptr[j+1];
+	chtmp = ptr[j];
+	ptr[j + 1] = chtmp;
+	ptr[j]=chtmp2;
+	return 1;			
+}
+
+int schr(char str[],char ch){
 	int i;
 	int idx = -1;
-	for (i=0; (str[i] != '\0') && (str[i] != ch); i++);
-		if (str[i] == ch)
-		idx = i;
+	for(i = 0;(str[i] != '\0')&&(str[i]!=ch);i++);
+		if(str[i] == ch)
+		idx=i;
 	return idx;
 }
 
-int stok(char str[], char *ptr[]) {
-	char *suf = str;
-	ptr[0] = str;
-	int i, j = 1;
+int stok(char str[], char *ptr[]){
+	char *suf=str;
+	ptr[0] = str; 
+	int i, j = 1; 
 	char delim = ' ';
-	while ((i = schr(suf, delim)) >= 0) {
+	while((i = schr(suf, delim))>=0){
 		suf[i] = '\0';
 		suf = suf + i + 1;
 		ptr[j] = suf;
@@ -25,10 +36,10 @@ int stok(char str[], char *ptr[]) {
 	return j;
 }
 
-int crop(char string[], char *fstring) {
+int crop(char string[], char *fstring){
 	int j = 0, i = 0;
-	while (string[j] != '\0') {
-		if ((string[j] >= 'A' && string[j] <= '?') || (string[j] >= 'a' && string[j] <= 'i') || (string[j] >= '?' && string[j] <= 'y') || string[j] == ' ') {
+	while (string[j] != '\0'){
+		if ((string[j] >= 'A' && string[j] <= '?') || (string[j] >= 'a' && string[j] <= 'i') || (string[j] >= '?' && string[j] <= 'y') || string[j]==' '){
 			fstring[i] = string[j];
 			i++;
 		}
@@ -38,40 +49,29 @@ int crop(char string[], char *fstring) {
 	return 1;
 }
 
-int sort(char *ptr[], int size) {
+int sort(char *ptr[], int size){
 	char *mstmp;
 	char *mstmp2;
-	char *chtmp;
-	char *chtmp2;
-	char A[] = "A,A,A,A,A,A,?,?,C,E,E,E,E,I,I,I,I,?,N,O,O,O,O,O,?,O,U,U,U,U,Y,?,?";
-	char a[] = "a,a,a,a,a,a,?,?,c,e,e,e,e,i,i,i,i,?,n,o,o,o,o,o,?,o,u,u,u,u,y,?,y";
-	int i = 0, j = 0;
-	for (i = 0; i < (size - 1); ++i) {//eiee?anoai io?iuo i?ioiaia ii ianneao aey iaoi?aaiey
-		for(j = 0; j < (size - 1); ++j) {
-			mstmp = ptr[j];
-			mstmp2 = ptr[j + 1];
-			if ((!(mstmp2[j] >= 'A' && mstmp2[j] <= '?') && !(mstmp[j] >= 'A' && mstmp[j] <= '?')) || ((mstmp2[j] >= 'A' && mstmp2[j] <= '?') && (mstmp[j] >= 'A' && mstmp[j] <= '?')))
-				if (mstmp2[0] < mstmp[0]) {
-						chtmp2 = ptr[j + 1];
-						chtmp = ptr[j];
-						ptr[j + 1] = chtmp;
-						ptr[j] = chtmp2;
-					}
-			if ((mstmp2[j] >= 'A' && mstmp2[j] <= '?') && !(mstmp[j] >= 'A' && mstmp[j] <= '?'))
-				if (mstmp2[0] < (mstmp[0] - ' ')) {
-						chtmp2 = ptr[j + 1];
-						chtmp = ptr[j];
-						ptr[j + 1] = chtmp;
-						ptr[j] = chtmp2;
-					}
-			if (!(mstmp2[j] >= 'A' && mstmp2[j] <= '?') && (mstmp[j] >= 'A' && mstmp[j] <= '?'))
-				if ((mstmp2[0] - ' ') < mstmp[0]) {
-						chtmp2 = ptr[j + 1];
-						chtmp = ptr[j];
-						ptr[j + 1] = chtmp;
-						ptr[j] = chtmp2;
-					}
+	char A[]="AAAAAA??CEEEEIIII?NOOOOO?OUUUUY??";//?-32 aoeaa ii ianneao
+	char a[]="aaaaaa??ceeeeiiii?nooooo?ouuuuy?y";
+	int i=0, j=0, k=0;
+	int x=-1, y=-1;
+	for (i=0; i < (size-1); ++i){//eiee?anoai io?iuo i?ioiaia ii ianneao aey iaoi?aaiey
+		for(j=0; j < (size-1); ++j){
+			mstmp=ptr[j];
+			mstmp2=ptr[j+1];
+			
+			for(k=0; k<33; k++){
+				if ((mstmp[0]==A[k]) || (mstmp[0]==a[k]))
+					x=k;
+				if ((mstmp2[0]==A[k]) || (mstmp2[0]==a[k]))
+					y=k;
+			}
+				if (y < x)
+					exchange(ptr, j);		
+				
 		}
 	}
+	
 	return 1;
 }
