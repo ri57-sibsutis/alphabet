2345bab7051bffe5fbaf8d3ad628863addf4054c
Korna <artem-koma@yandex.ru>
2016-05-22 01:23:56 +0600

    main program file has only interaction with other functions now

--- src/main.c
+++ src/main.c
@@ -1,43 +1,20 @@
-#include "functions.h"
-#include "interface.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
-#include <windows.h>
+#include "functions.h"
+#include "interface.h"
 
-int main()//aaia iacaaiey i?iecaiaeony ia aiaeeeneii oieuei
+int main()
 {	
-   setlocale(LC_ALL, "Rus");
-    int j = 0, size = 1, a = 0;
-    char string[260];
-    char fstring[260];
-    char tmp[260];
-    char namein[260];
-    printf("Aia?i ii?aeiaaou a i?ia?aiio Aeoaaeo.\n");
-	printf("Aaaaeoa iacaaiea oaeea aey aaiaa no?iee:");
-	scanf("%s", &tmp);
-	OemToChar(tmp, namein);
-	FILE *fp = fopen (namein, "r");
-	if(fp == NULL)
-		printf("\nIoeaea: oaee iaei??aeoai\n");
-	for (j=0; j<260; j++){
-		fscanf (fp, "%c", &string[j]);
-		if (string[j]=='\n')
-			string[j] = '\0';
-	}
-	fclose(fp);	
-	printf("%s", string);
-	for (j = 0; string[j] != '\0'; j++){
-		if (string[j] == ' ')
-			size++;
-	}
+	int j = 0, size = 0, a = 0;
+	char string[260];
+	char fstring[260];
+	size = interact(string) + 1;
+	if (size == -1)
+		return 0;
 	char *ptr[size];
-	printf("\n?enei neia:%d\n", size);
 	crop(string, fstring);
 	stok(fstring, ptr);
 	sort(ptr, size);
-	printf("Iiaay no?iea:");
-	for (j = 0; j < size; j++)
-		printf(" %s", ptr[j]);
-    return 0;
+	out(ptr, size);
+	return 0;
 }
