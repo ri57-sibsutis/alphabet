c3b982e72b07c1368b1a195c7a38d501c62ba573
Korna <artem-koma@yandex.ru>
2016-05-22 00:54:33 +0600

    cleared from useless stuff and rebuilded

--- src/main.c
+++ src/main.c
@@ -2,42 +2,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <windows.h>
 
-int main()//ввод названия производится на английском только
-{
-	setlocale(LC_ALL, "Rus");
-	int j = 0, size = 1, a = 0;
-	char string[260];
-	char fstring[260];
-	char name[260] = "file.txt";
-	char name2[260];
-	printf("Добро пожаловать в программу Алфавит.\n");
-//	printf("Введите название файла для ввода строки:");
-//	scanf("%s", &name);
-	FILE *fp = fopen(name, "r");
-	if (fp == NULL)
-		printf("\nОшибка: файл некорректен\n");
-	for (j = 0; j < 260; j++) {
-		fscanf(fp, "%c", &string[j]);
-		if (string[j] == '\n')
+int main()//aaia iacaaiey i?iecaiaeony ia aiaeeeneii oieuei
+{	
+   setlocale(LC_ALL, "Rus");
+    int j = 0, size = 1, a = 0;
+    char string[260];
+    char fstring[260];
+    char tmp[260];
+    char namein[260];
+    printf("Aia?i ii?aeiaaou a i?ia?aiio Aeoaaeo.\n");
+	printf("Aaaaeoa iacaaiea oaeea aey aaiaa no?iee:");
+	scanf("%s", &tmp);
+	OemToChar(tmp, namein);
+	FILE *fp = fopen (namein, "r");
+	if(fp == NULL)
+		printf("\nIoeaea: oaee iaei??aeoai\n");
+	for (j=0; j<260; j++){
+		fscanf (fp, "%c", &string[j]);
+		if (string[j]=='\n')
 			string[j] = '\0';
 	}
-	fclose(fp);
+	fclose(fp);	
 	printf("%s", string);
-	for (j = 0; string[j] != '\0'; j++) {
+	for (j = 0; string[j] != '\0'; j++){
 		if (string[j] == ' ')
 			size++;
 	}
 	char *ptr[size];
-	for (j = 0; string[j] != '\0'; j++)//в порядке возрастания значения буквы в интовом коде
-		if ((string[j] >= 'А' && string[j] <= 'Я') || (string[j] >= 'а' && string[j] <= 'п') || (string[j] >= 'р' && string[j] <= 'я')) {
-			a++;
-		}
-	printf("\nЧисло слов:%d\n", size);
+	printf("\n?enei neia:%d\n", size);
 	crop(string, fstring);
 	stok(fstring, ptr);
 	sort(ptr, size);
-	printf("Новая строка:");
+	printf("Iiaay no?iea:");
 	for (j = 0; j < size; j++)
 		printf(" %s", ptr[j]);
     return 0;
