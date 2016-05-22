3feee8b1394239a5f2cd448980a2d97e7558f707
Korna <artem-koma@yandex.ru>
2016-05-22 19:46:22 +0600

    updated interface

--- src/main.c
+++ src/main.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "functions.h"
-#include "interface.h"
 
 int main()
 {	
-    	int j = 0, size = 0, a = 0;
-    	char string[260];
-    	char fstring[260];
-	size = interact(string) + 1;
+    int size = 0, a = 0;
+    char string[260];
+    char fstring[260];
+       printf("Choose your interface language:\nType 1 for English.\nType 2 for Russian.\n");
+    scanf("%d", &a);
+    if ((a != 1) && (a != 2)){
+    	printf("Wrong language input.");
+    	return -1;
+    }
+	size = interact(string, a) + 1;
 	if (size == 0)
 		return 0;
 	char *ptr[size];
 	crop(string, fstring);
 	stok(fstring, ptr);
 	sort(ptr, size);
-	out(ptr, size);
-    	return 0;
+	out(ptr, size, a);
+	getchar();
+	getchar();
+    return 0;
 }
