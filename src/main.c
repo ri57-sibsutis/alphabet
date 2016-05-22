78ee87af92ebc92a50776ac2c4798e4ddf0952f0
Korna <artem-koma@yandex.ru>
2016-05-22 16:36:02 +0600

    updated error notification

--- src/main.c
+++ src/main.c
@@ -5,16 +5,16 @@
 
 int main()
 {	
-	int j = 0, size = 0, a = 0;
-	char string[260];
-	char fstring[260];
+    	int j = 0, size = 0, a = 0;
+    	char string[260];
+    	char fstring[260];
 	size = interact(string) + 1;
-	if (size == -1)
+	if (size == 0)
 		return 0;
 	char *ptr[size];
 	crop(string, fstring);
 	stok(fstring, ptr);
 	sort(ptr, size);
 	out(ptr, size);
-	return 0;
+    	return 0;
 }
