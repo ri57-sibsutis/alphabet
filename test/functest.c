310efb50a838589d38f399b9c624c8ec305e13dd
Korna <artem-koma@yandex.ru>
2016-05-23 17:23:43 +0600

    added new tests

--- test/functest.c
+++ test/functest.c
@@ -2,6 +2,39 @@
 #include <functions.h>
 #include <locale.h>
 
+CTEST(equation_suite, crop_of_symbols)
+{
+	//Given
+	char string[] = "...sad";
+	
+	//When
+	char fstring[260];
+	crop(string, fstring);
+
+	//Then
+	const int a = fstring[0];
+	const int ch = 's';
+	ASSERT_EQUAL(a, ch);
+
+}
+
+CTEST(equation_suite, crop_of_symbols_rus)
+{
+	setlocale(LC_ALL, "Rus");
+	//Given
+	char string[] = "הטה";
+	
+	//When
+	char fstring[260];
+	crop(string, fstring);
+	const int a = fstring[2];
+
+	//Then
+	const int ch = 'ה';
+	ASSERT_EQUAL(a, ch);
+
+}
+
 CTEST(strings_process, search_of_rus_word)
 {
 	setlocale(LC_ALL, "Rus");
@@ -83,4 +116,109 @@ CTEST(equation_suite, search_of_eng_word2)
 	//Then
 	ASSERT_EQUAL(rv, a);
 
+}
+
+CTEST(equation_suite, stok_multiple)
+{
+	//Given
+	char string[]= "aaaay lmaaao";
+	char *ptr[2];
+	int size = stok(string, ptr);
+	char *tmp1 = ptr[0];
+	char *tmp2 = ptr[1];
+
+	//When
+	const int a = tmp2[0];
+	const int b = 'l';
+	const int c = 'a';//change
+	const int d = tmp1[0];
+	const int exp_size = 2;
+
+	//Then
+	ASSERT_EQUAL(b, a);
+	ASSERT_EQUAL(d, c);
+	ASSERT_EQUAL(exp_size, size);
+}
+
+CTEST(equation_suite, stok_single)
+{
+	//Given
+	char string[]= "haallo";
+	char *ptr[1];
+	stok(string, ptr);
+	char *tmp1 = ptr[0];
+	//When
+	
+	const int expct = 'h';
+	const int real = tmp1[0];
+
+	//Then
+	ASSERT_EQUAL(real, expct);
+}
+
+CTEST(equation_suite, sort_eng)
+{
+	//Given
+	char string[]= "b a c";
+	char *ptr[3];
+	stok(string, ptr);
+	sort(ptr, 3);
+	char *tmp1 = ptr[0];
+	char *tmp2 = ptr[1];
+	char *tmp3 = ptr[2];
+	//When
+	
+	const int expct_a = 'a';
+	const int real_a = tmp1[0];
+	const int expct_b = 'b';
+	const int real_b = tmp2[0];
+	const int expct_c = 'c';
+	const int real_c = tmp3[0];
+
+	//Then
+	ASSERT_EQUAL(expct_a, real_a);
+	ASSERT_EQUAL(expct_b, real_b);
+	ASSERT_EQUAL(expct_c, real_c);
+}
+
+CTEST(equation_suite, exchange_of_eng_words)
+{
+	//Given
+	char string[]= "lmao ay";
+	char *ptr[2];
+	stok(string, ptr);
+	exchange(ptr, 0);
+	char *tmp1 = ptr[0];
+	char *tmp2 = ptr[1];
+	//When
+	
+	const int real = tmp1[0];
+	const int expct = 'a';
+
+	//Then
+	ASSERT_EQUAL(expct, real);
+
+}
+
+CTEST(equation_suite, exchange_of_rus_words)
+{
+	setlocale(LC_ALL, "Rus");
+	//Given
+	char *ptr[2];
+	ptr[0]="במ";
+	ptr[1]="ÿחü";
+	ptr[2]="ר";
+	exchange(ptr, 0);
+	char *tmp1 = ptr[1];
+	char *tmp = ptr[0];
+	//When
+	
+	const int real = tmp[0];
+	const int expct = 'ÿ';
+	const int real_1 = tmp1[0];
+	const int expct_1 = 'ב';
+
+	//Then
+	ASSERT_EQUAL(real_1, expct_1);
+	ASSERT_EQUAL(real, expct);
 }
